be8c8700e7d82d63e52d8a204d00a0dd97478d26
Alejandro <alejandro@fakemail.com>
2023-10-30 07:30:31 +0100

    Fixed search value

--- src/widgets/FilterWidget.py
+++ src/widgets/FilterWidget.py
@@ -70,7 +70,8 @@ class FilterWidget(QWidget, ListenerNode):
         self.send_event(self.table_name, 'refresh_rows', self.filter)
         self.send_event('view-charts-screen', 'reload_charts', self.filter)
 
-    def change_search_value(self, search_value):
+    def change_search_value(self, search_value = None):
         self.filter['search_value'] = search_value
 
+        print(f'>>>> Changing search value to "{search_value}"')
         self.send_event(self.table_name, 'refresh_rows', self.filter)
